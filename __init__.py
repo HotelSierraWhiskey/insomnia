e5d73b3acf95425f68d105efaea6b985a54aaf06
HotelSierraWhiskey <winstontothemoon@gmail.com>
2023-11-21 17:32:04 -0500

    added pre auth

--- __init__.py
+++ __init__.py
@@ -4,19 +4,44 @@ from functools import partial
 import threading
 
 
-class card_task(CardMonitor):
-    def __init__(self, func):
+class Insomnia(CardMonitor):
+    def __init__(self, config=None):
         super().__init__()
-        self._func = func
+        self.config = config
         self.scan_event = threading.Event()
 
-    def event_callback(self, result):
+    def threading_event_callback(self, result):
         self.observer_result = result
         self.scan_event.set()
 
-    def __call__(self, *args, **kwargs):
-        func = partial(self._func, *args, **kwargs)
-        observer = Observer(func, self.event_callback)
-        self.addObserver(observer)
-        self.deleteObserver(observer)
-        return observer.result
+    def basic(self, *args, **kwargs):
+        def decorator(func):
+            def wrapper():
+                callback = partial(func, *args, **kwargs)
+                observer = Observer(
+                    callback=callback, threading_event=self.threading_event_callback
+                )
+                self.addObserver(observer)
+                self.deleteObserver(observer)
+                return observer.result
+
+            return wrapper
+
+        return decorator
+
+    def authenticate(self, *args, **kwargs):
+        def decorator(func):
+            def wrapper():
+                callback = partial(func, *args, **kwargs)
+                observer = Observer(
+                    callback=callback,
+                    auth=self.config,
+                    threading_event=self.threading_event_callback,
+                )
+                self.addObserver(observer)
+                self.deleteObserver(observer)
+                return observer.result
+
+            return wrapper
+
+        return decorator
